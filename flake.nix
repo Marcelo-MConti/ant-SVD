6c9cbf8f137f6c8c9e9eefe3c400bb13a59adfc2
goll <joaog.bastos@protonmail.ch>
2026-06-08 13:31:12 -0300

    Scaffolding

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  description = "Monitoramento de emissões de carbono corporativas";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs = inputs @ {flake-parts, ...}:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = ["x86_64-linux"];
+
+      perSystem = {
+        config,
+        pkgs,
+        lib,
+        system,
+        ...
+      }: let
+        py = pkgs.python313Packages;
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = [
+            py.python
+
+            pkgs.uv
+            pkgs.ty
+            pkgs.ruff
+          ];
+
+          shellHook = ''
+            REPO_ROOT=$(git rev-parse --show-toplevel)
+
+            cd "$REPO_ROOT"
+            
+            uv sync
+            . .venv/bin/activate
+          '';
+        };
+      };
+    };
+}
