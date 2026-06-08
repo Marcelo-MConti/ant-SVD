d98a058fb1104ad18e30434ed18b929edff325d7
Joao Pedro Castelli <jotapcastelli@gmail.com>
2026-06-08 17:38:52 -0300

    add jacobi_rotation

--- src/svd/jacob.py
+++ src/svd/jacob.py
@@ -2,8 +2,8 @@
 import numpy as np
 import numpy.typing as npt
 
+# find maximum value in symetric matrix
 def Find_Max_Symetric(A: npt.NDArray[np.float64]) -> tuple[np.float64, int, int]:
-    # find maximum value in symetric matrix
     max: np.float64 = A[0][0]
     index = 0
     p: int = 0
@@ -21,14 +21,51 @@ def Find_Max_Symetric(A: npt.NDArray[np.float64]) -> tuple[np.float64, int, int]
 
         index = index + 1
     return (max, p, q)
+#end
+
+# calculate the cos and sin(phi) to rotate the matrix A in a way that A[p][q] == 0
+def Calculate_Trigonometric(A: npt.NDArray[np.float64], p: int, q: int) -> tuple[int, int]:
+    big_phi = (A[q][q] - A[p][p]) / (2 * A[p][q])
+    tang = 1
+    if big_phi > 1e-10:
+        tang = 1 / (big_phi + np.sign(big_phi) * np.sqrt(pow(big_phi, 2) + 1))
+
+    cosi = 1 / (pow(tang, 2) + 1)
+    sino = tang * cosi
+        
+    return (cosi, sino)
 
 
 # A needs to be symetric
-def Jacobi_Decomposition(A: npt.NDArray[np.float64]) -> npt.NDArray[np.float64]:
+def Jacobi_Decomposition(A: npt.NDArray[np.float64], tol = 1e-9) -> tuple[npt.NDArray[np.float64], npt.NDArray[np.float64]]:
+    # U is the rotation matrix
+    U = np.identity(A.shape[0])
+    # V is the product of all rotation matrices
+    V = np.identity(A.shape[0])
+    # Ak is the k-th iteration of A, approximately a diagonal matrix
+    Ak = np.identity(A.shape[0])
+
     (max, p, q) = Find_Max_Symetric(A)
+    while max > tol:
+        # cos(phi) and sin(phi)
+        (c, s) = Calculate_Trigonometric(A, p, q)
 
-    return A
+        U[p][p] = c
+        U[p][q] = s
+        U[q][p] = -s
+        U[q][q] = c
+
+        # V is the product of all rotation matrices
+        V = V.dot(U)
+        # calculate the roation Vt*A*V
+        Ak = V.T.dot(A.dot(V))
+
+        # revert U to the identity matrix
+        U[p][p] = 0
+        U[p][q] = 0
+        U[q][p] = 0
+        U[q][q] = 0
+        (max, p, q) = Find_Max_Symetric(Ak)
+
+    return (Ak, V)
 #end
-
-
-Jacobi_Decomposition(np.array([[1, 2, 3], [4, 5, 6]]))
