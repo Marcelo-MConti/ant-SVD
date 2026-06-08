bec0fcc48bedeafb2b8f23147ebf12ff94570f49
Joao Pedro Castelli <jotapcastelli@gmail.com>
2026-06-08 16:03:15 -0300

    imcomplete find maximum value

--- src/svd/jacob.py
+++ src/svd/jacob.py
@@ -1,7 +1,20 @@
 # Jacó, filho de Isaac, filho de Abraão, teve 12 filhos.
-from numpy import matrix
+import numpy as np
 
 # A needs to be symetric
-def Jacobi_Decomposition(A: matrix) -> matrix:
+def Jacobi_Decomposition(A: np.matrix) -> np.matrix:
+    max: np.float64 = A[0][0]
+    p: int = 0
+    q: int = 0
+
+    for row in np.nditer(A, flags=["external_loop"], order='C'):
+        print(row)
+        wow = abs(row[0].max())
+        if max < wow:
+            max = wow
+        #end
     return A
-#exit
+#end
+
+
+Jacobi_Decomposition(np.matrix([[1, 2, 3], [4, 5, 6]]))
