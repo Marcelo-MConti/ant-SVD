be78f09e150ddbb9ba842e4c526c41adc08ab0f1
Joao Pedro Castelli <jotapcastelli@gmail.com>
2026-06-09 00:12:46 -0300

    comment jacob

--- src/svd/jacob.py
+++ src/svd/jacob.py
@@ -2,8 +2,9 @@ import numpy as np
 import numpy.typing as npt
 
 
-# find maximum value in symetric matrix aside from the diagonal
-def Find_Max_Symetric(A: npt.NDArray[np.float64]) -> tuple[np.float64, int, int]:
+# Find maximum value in symmetric matrix aside from the diagonal
+# Return (absolute of the biggest value, its line and column indexes)
+def Find_Max_Symmetric(A: npt.NDArray[np.float64]) -> tuple[np.float64, int, int]:
     max = abs(A[0][1])
     p: int = 0
     q: int = 1
@@ -20,13 +21,14 @@ def Find_Max_Symetric(A: npt.NDArray[np.float64]) -> tuple[np.float64, int, int]
     return (max, p, q)
 
 
-# calculate the cosx and sinx to rotate the matrix A in a way that A[p][q] == 0
+# Calculate the cosine and sine to rotate the matrix A in a way that A[p][q] == 0
+# Return their values
 def Calculate_Trigonometric(A: npt.NDArray[np.float64], p: int, q: int) -> tuple[float, float]:
+    # These equations are derived from Ut A U
     phi = (A[q][q] - A[p][p]) / (2 * A[p][q])
     tang = 1
     if abs(phi) > 1e-15:
         tang = 1 / (phi + np.sign(phi) * np.sqrt((phi ** 2) + 1))
-
 
     cosx = 1 / np.sqrt((tang ** 2) + 1)
     sinx = tang * cosx
@@ -34,7 +36,10 @@ def Calculate_Trigonometric(A: npt.NDArray[np.float64], p: int, q: int) -> tuple
     return (cosx, sinx)
 
 
-# A needs to be symetric
+# Apply the Jacobi iterative method to extract the eigenvalues and eigenvectors of the matrix A
+# A needs to be symetric. There are two stopping conditions being used.
+# Thy are: maximum value outside the diagonal in the Ak matrix and number of iterations.
+# Return (array of the eigenvalues, matrix of the eigenvectors)
 def Jacobi_Decomposition(A: npt.NDArray[np.float64], tol = 1e-15, kmax = 1000) -> tuple[npt.NDArray[np.float64], npt.NDArray[np.float64]]:
     # U is the rotation matrix
     U = np.identity(A.shape[0])
@@ -42,11 +47,11 @@ def Jacobi_Decomposition(A: npt.NDArray[np.float64], tol = 1e-15, kmax = 1000) -
     # V is the product of all rotation matrices
     V = np.identity(A.shape[0])
 
-    # Ak is the k-th iteration of A, approximately a diagonal matrix
+    # Ak is the k-th iteration of A, approximating a diagonal matrix
     Ak = np.copy(A)
     k = 0
 
-    (max, p, q) = Find_Max_Symetric(A)
+    (max, p, q) = Find_Max_Symmetric(A)
     while max > tol and k < kmax:
         # cos(phi) and sin(phi)
         (c, s) = Calculate_Trigonometric(Ak, p, q)
@@ -60,7 +65,7 @@ def Jacobi_Decomposition(A: npt.NDArray[np.float64], tol = 1e-15, kmax = 1000) -
         # V is the product of all rotation matrices
         V = V @ U
 
-        # calculate the roation Vt*A*V
+        # calculate the rotation Vt*A*V
         Ak = V.T @ A @ V
 
         # revert U to the identity matrix
@@ -70,7 +75,7 @@ def Jacobi_Decomposition(A: npt.NDArray[np.float64], tol = 1e-15, kmax = 1000) -
         U[q][q] = 1
 
         # next iteration
-        (max, p, q) = Find_Max_Symetric(Ak)
+        (max, p, q) = Find_Max_Symmetric(Ak)
         k = k + 1
 
     return (np.diag(Ak), V)
