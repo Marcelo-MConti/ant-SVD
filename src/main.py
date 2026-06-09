686465342326a238899a82119872c37e72da62b8
goll <joaog.bastos@protonmail.ch>
2026-06-08 21:55:15 -0300

    mask works ig

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 from argparse import ArgumentParser
-
 import itertools
+
+import os
+import shutil
 
 import numpy as np
 
@@ -11,8 +13,6 @@ import skvideo.utils
 import skimage
 
 import scipy
-
-from matplotlib import pyplot as plt
 
 from svd.manual_svd import SVD_decomposition
 
@@ -31,7 +31,7 @@ def main():
     )
 
     parser.add_argument(
-        "-C", "--transparent-color",
+        "-c", "--transparent-color",
         help="Color to replace transparent sections with (RGB hex format)",
         type=str,
         default="#ff60d0"
@@ -66,23 +66,83 @@ def main():
     )
 
     parser.add_argument(
+        "-t", "--threshold",
+        help="Threshold to use when quantizing the mask",
+        type=float,
+        default=0.2
+    )
+
+    parser.add_argument(
+        "-M", "--write-masks",
+        help="Write the masks used to remove the background to separate image files",
+        action="store_true",
+        default=False
+    )
+
+    parser.add_argument(
+        "-C", "--chunks",
+        help="Number of chunks to process",
+        type=int,
+        default=5
+    )
+
+    parser.add_argument(
         "filename",
         help="Path to a video file to be processed"
     )
 
     args = parser.parse_args()
 
-    if args.batch_size < 15 or args.batch_size > 60:
-        raise ValueError("Invalid value for batch_size, must be between 30 and 60 frames")
+    BATCH_MIN = 15
+    BATCH_MAX = 60
+
+    if args.batch_size < BATCH_MIN or args.batch_size > BATCH_MAX:
+        raise ValueError(f"Invalid value for batch_size, must be between {BATCH_MIN} and {BATCH_MAX} frames")
+
+    if args.transparent_color[:1] != "#":
+        raise ValueError("Invalid value for transparent_color")
+
+    r = int(args.transparent_color[1:3], base=16)
+    g = int(args.transparent_color[3:5], base=16)
+    b = int(args.transparent_color[5:7], base=16)
+
+    trans_yuv = skimage.color.rgb2yuv(np.array([[[r, g, b]]], dtype=np.float64) / 255.0)[0, 0, :]
 
     # Iterador dos frames do vídeo no formato YUV444P
     video_gen = skvideo.io.vreader(args.filename)
 
-    for frames in itertools.batched(video_gen, args.batch_size):
-        # XXX: Recreating a matrix like this every time is expensive
-        frame_mat = np.ndarray(shape=(args.height * args.width, args.batch_size))
+    def rm_r(path):
+        try:
+            shutil.rmtree(path)
+        except FileNotFoundError:
+            pass
 
-        for i in range(args.batch_size):
+    def mkdir(path):
+        try:
+            os.mkdir(path)
+        except FileExistsError:
+            pass
+
+    rm_r("output/masks")
+    rm_r("output/videos")
+
+    mkdir("output")
+    mkdir("output/masks")
+    mkdir("output/videos")
+
+    # Retorna um handle que pode ser usado para montar o vídeo de saída, frame a frame, no formato YUV444P
+    video_out = skvideo.io.FFmpegWriter(f"output/videos/{os.path.basename(args.filename)}")
+
+    cur_chunk = 0
+
+    for frames in itertools.batched(video_gen, args.batch_size):
+        batch_size = len(frames)
+        (orig_height, orig_width, n_chan) = frames[0].shape
+
+        # XXX: Recreating a matrix like this every time is expensive
+        frame_mat = np.ndarray(shape=(args.height * args.width, batch_size))
+
+        for i in range(batch_size):
             # frames[i].shape = (height, width, n_chan)
             #
             # YUV444P has three channels: Y (luma), U (blue difference), V (green difference)
@@ -115,18 +175,41 @@ def main():
 
         dyn_mat = np.abs(frame_mat - sta_mat)
 
-        for i in range(args.batch_size):
+        for i in range(batch_size):
             # Process each frame mask
             frame_mask = np.reshape(dyn_mat[:, i], (args.height, args.width))
 
             m = np.max(frame_mask)
             print(f"m={m}")
-            bin_mask = np.round(frame_mask / m + 0.3)
+            bin_mask = np.round(frame_mask * (0.5 / args.threshold) / m)
 
             filled_mask = scipy.ndimage.binary_fill_holes(bin_mask)
+            filled_mask_resized = skimage.transform.resize(filled_mask, (orig_height, orig_width))
 
-            plt.imshow(filled_mask, cmap="gray", vmax=1.)
-        plt.show()
+            if args.write_masks:
+                skimage.io.imsave(f"output/masks/{batch_size * cur_chunk + i}.png", filled_mask_resized)
+
+            masked_frame = np.zeros_like(frames[i])
+
+            # Convolve/Apply the mask to the frame to remove the background
+            for j in range(n_chan):
+                masked_frame[:, :, j] = frames[i][:, :, j] * filled_mask_resized
+
+            # Invert the mask
+            # filled_mask = 1 - filled_mask
+
+            # # Replace the background color in the masked frame with args.transparent_color
+            # for j in range(n_chan):
+            #     masked_frame[:, :, j] += np.full((orig_height, orig_width), trans_yuv[j] * 255, dtype=np.uint8) * filled_mask_resized
+
+            video_out.writeFrame(masked_frame)
+
+        cur_chunk += 1
+
+        if cur_chunk > args.chunks:
+            break
+
+    video_out.close()
 
 
 if __name__ == "__main__":
